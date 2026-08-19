5b31b45ffada51917d52da53dc4bcb93a26ca4d0
yeowonh <54229099+yeowonh@users.noreply.github.com>
2026-08-19 10:41:05 +0900

    Keep review preview out of search

--- sites-worker/index.js
+++ sites-worker/index.js
@@ -1,7 +1,20 @@
+function preventIndexing(response) {
+  const headers = new Headers(response.headers);
+  headers.set('X-Robots-Tag', 'noindex, nofollow, noarchive, nosnippet');
+
+  return new Response(response.body, {
+    status: response.status,
+    statusText: response.statusText,
+    headers,
+  });
+}
+
 export default {
   async fetch(request, env) {
     const response = await env.ASSETS.fetch(request);
-    if (response.status !== 404 || !['GET', 'HEAD'].includes(request.method)) return response;
+    if (response.status !== 404 || !['GET', 'HEAD'].includes(request.method)) {
+      return preventIndexing(response);
+    }
 
     const url = new URL(request.url);
     const cleanPath = url.pathname === '/' ? '/index' : url.pathname.replace(/\/$/, '');
@@ -9,14 +22,14 @@ export default {
     if (!cleanPath.split('/').pop()?.includes('.')) {
       url.pathname = `${cleanPath}.html`;
       const htmlResponse = await env.ASSETS.fetch(new Request(url, request));
-      if (htmlResponse.status !== 404) return htmlResponse;
+      if (htmlResponse.status !== 404) return preventIndexing(htmlResponse);
     }
 
     url.pathname = '/404.html';
     const notFound = await env.ASSETS.fetch(new Request(url, request));
-    return new Response(notFound.body, {
+    return preventIndexing(new Response(notFound.body, {
       status: 404,
       headers: notFound.headers,
-    });
+    }));
   },
 };
